31f90417714817e81dbd165eb399bb378b68cee0
Sergej Lust <sergejlust@Sergejs-MBP.fritz.box>
2022-04-01 10:29:18 +0200

    added livedemos app architecture and name=main thing

---
+++ livedemos/first_script.py
@@ -0,0 +1,8 @@
+import second_script
+
+print('1'*60)
+print(__name__)
+
+# if __name__ == "__main__":
+#     print('1'*60)
+#     print(__name__)
---
+++ livedemos/second_script.py
@@ -0,0 +1,8 @@
+# import first_script
+
+print('2'*60)
+print(__name__)
+
+# if __name__ == "__main__":
+#     print('2'*60)
+#     print(__name__)
